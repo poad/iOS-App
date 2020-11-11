1cd08ac1f1904d3a89bc234ba7cfc6938dcd3838
Kenji Saito <ken-yo@mbr.nifty.com>
2020-11-11 18:54:38 +0900

    Initial commit

---
+++ HelloApp/HelloApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloApp
+//
+//  Created by 齊藤健司 on 2020/09/07.
+//  Copyright © 2020 齊藤健司. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("こんにちは")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
